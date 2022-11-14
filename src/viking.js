ba68399906d7063223478ac5b8278c109507be9c
Estefania Egea <estefania.egcal@gmail.com>
2022-11-14 02:08:19 +0100

    empezado

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,45 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor (health, strength) {
+    this.health = health; 
+    this.strength = strength;}
+    attack() {
+        return this.strength
+    } 
+    receiveDamage() {
+
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor (name, health, strength) {
+        super(health, strength)
+        this.name = name;
+}     
+    attack() {
+        return this.strength
+    } 
+    receiveDamage() {
+        
+    }
+    battleCry() {
+        return ('Odin Owns You All!')
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+    constructor (health, strength) {
+        super (health, strength)
+    }
+    attack() {
+        return this.strength
+    } 
+    receiveDamage() {
+        
+    }
+}
 
 // War
 class War {}
